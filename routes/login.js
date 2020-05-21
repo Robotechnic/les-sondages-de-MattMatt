0f4384a9c427f950587e3de79b4a92d9e03f7256
Robotechnic <robotechnic.developeur@gmail.com>
2020-05-21 20:01:36 +0200

    add LoIn and SignIn pages

--- routes/login.js
+++ routes/login.js
@@ -3,10 +3,10 @@ const router = express.Router()
 
 module.exports = (db) =>{
 	router.get("/login",(req,res)=>{
-		
+		res.render("logIn.ejs")
 	})
 	router.get("/signIn",(req,res)=>{
-		
+		res.render("signIn.ejs")
 	})
 	return router
 }
